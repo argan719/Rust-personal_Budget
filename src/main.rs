bb28197720263cfc72af95d604f7e4de69994d86
argan719 <argan719@naver.com>
2024-11-22 15:53:49 +0900

    기본 로직은 구현했으나, SQLite 및 diesel 관련 환경 구축 에러 발생

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,40 @@ use serde_json::json;
 use rocket_sync_db_pools::database;
 use rocket::routes;
 
+#[macro_use]
+extern crate diesel;
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket::routes;
+
 
 
 #[database("personal_budget")]
 struct BudgetDb(diesel::SqliteConnection);
 
+
+
+
+
+
+
+
+
+
+######
+
+
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate diesel;
+
+use diesel::prelude::*;
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket_sync_db_pools::database;
+
+#[database("budget_db")]
+struct BudgetDb(diesel::SqliteConnection);
 
 // 데이터 베이스 스키마 작성
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
@@ -18,18 +47,44 @@ struct Transaction {
     description: Option<String>,
 }
 
+#[derive(Insertable, Queryable)]
+#[diesel(table_name = transactions)]
+struct DbTransaction {
+    id: Option<i32>,
+    description: String,
+    amount: f64,
+}
 
-// Rocket 플레임워크 
-// CRUD API
+#[derive(diesel::table)]
+#[diesel(table_name = "transactions")]
+pub struct Transactions;
 
 // CRUD 핸들러
-#[rocket::post("/transaction", format = "json", data = "<transaction>")]
-async fn create_transaction(transaction: rocket::serde::json::Json<Transaction>) -> &'static str {
-    "Transaction added successfully!"
+#[post("/transaction", data = "<transaction>")]
+async fn create_transaction(
+    db: BudgetDb,
+    transaction: Json<Transaction>,
+) -> Json<&'static str> {
+    let new_transaction = DbTransaction {
+        id: None,
+        description: transaction.description.clone(),
+        amount: transaction.amount,
+    };
+
+    db.run(move |conn| {
+        diesel::insert_into(transactions::table)
+            .values(&new_transaction)
+            .execute(conn)
+    })
+    .await
+    .map(|_| Json("Transaction added successfully!"))
+    .unwrap_or(Json("Failed to add transaction."))
 }
 
 // 라우트 정의
-#[rocket::launch]
+#[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![create_transaction])
+    rocket::build()
+        .attach(BudgetDb::fairing())
+        .mount("/", routes![create_transaction])
 }
